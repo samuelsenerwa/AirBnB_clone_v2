c96a5cd56edf1d2d6336be67700ef17cc5976fb8
Julius-Okeyo <jaykopiyo@gmail.com>
2023-04-20 04:09:43 -0700

    Rolled back on corrections

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,13 +7,13 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER
     IF NOT EXISTS 'hbnb_test'@'localhost'
-    IDENTIFIED WITH 'hbnb_test_pwd';
+    IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
    ON `hbnb_test_db`.*
    TO 'hbnb_test'@'localhost'
-   IDENTIFIED WITH 'hbnb_test_pwd';
+   IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT
    ON `performance_schema`.*
    TO 'hbnb_test'@'localhost'
-   IDENTIFIED WITH 'hbnb_test_pwd';
+   IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
