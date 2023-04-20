37e7cb3c32855c9e4749b869198bd5f678da5b07
Julius-Okeyo <jaykopiyo@gmail.com>
2023-04-20 04:03:04 -0700

    Corrected syntax on script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,13 +7,13 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
     IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
+    IDENTIFIED WITH 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES
    ON `hbnb_dev_db`.*
    TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+   IDENTIFIED WITH 'hbnb_dev_pwd';
 GRANT SELECT
    ON `performance_schema`.*
    TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+   IDENTIFIED WITH 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
