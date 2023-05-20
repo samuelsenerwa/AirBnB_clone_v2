f763453e17b4374bb1df2fa732a816b83b44cfdc
samuelsenerwa <samuelsenerwa@gmail.com>
2023-05-19 23:24:37 -0700

    pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
+
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'.
@@ -24,4 +25,3 @@ def hbnb():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-
